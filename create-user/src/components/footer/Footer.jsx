608b7737baa26e6b852d25d6e179b056bedf23df
Umarbek <yoldashovumar0@gmail.com>
2026-05-11 16:45:27 +0500

    Completed

---
+++ create-user/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All Right Reserved. <a href="https://github.com/UmarbekY">Umarbek Yuldashov</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
